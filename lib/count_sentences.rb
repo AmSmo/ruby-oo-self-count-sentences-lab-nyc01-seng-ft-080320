155e3450b27eadcbc1afa15c93adfb37f29c657a
AmSmo <adam.smolenski@gmail.com>
2020-08-04 16:49:14 -0400

    Regex is so much prettier in python

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]+/)
+    puts sentences
+    if sentences == nil
+      return 0
+    else
+      return sentences.count
+    end
   end
 end
